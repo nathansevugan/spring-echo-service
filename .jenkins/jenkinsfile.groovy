fbf7f68eb1446ea867f33bad3bbf7efeaf9a5f74
Sevuganchetty, Ranganathan <ranganathan.sevuganchetty@sabre.com>
2018-12-03 13:54:59 -0600

    added build configuration to unify application build and image creation

---
+++ .jenkins/jenkinsfile.groovy
@@ -0,0 +1,98 @@
+#!groovy
+package pipelines
+
+pipeline {
+    agent {
+        node { label 'jenkins-build-pipeline' }
+    }
+
+    environment {
+//        JAVA_HOME = "/usr/lib/jdk10/jdk-10.0.2"
+    }
+    options {
+        // set a timeout of 20 minutes for this pipeline
+        timeout(time: 2, unit: 'MINUTES')
+    }
+    stages {
+
+        stage('Info') {
+            steps {
+                script {
+                    //Global variable section
+                    //project that holds images
+                    projectId = "dev"
+
+                    //Staging reposiroty credentials [aprm cicd]
+//                    repositoryCredentials = "HmVFwNO3:etaWeJoi/639yLO7RnBOrHs6Miy1ihX2joLNl0IOd5rX"
+//
+//                    shortCommit = sh(returnStdout: true, script: "git log -n 1 --pretty=format:'%h'").trim()
+//                    branch = scm.branches[0].name
+//                    buildNumber = env.getEnvironment().get("BUILD_NUMBER")
+//                    repoName = sh(script: 'git remote -v|grep \'(fetch)\'|awk \'{print $2}\' | gawk \'match($0, /([^/]+).git$/, ary) {print ary[1]}\'', returnStdout: true).trim()
+
+//                    muleVersion = "4.3.2.1"
+//                    imageSuffix = muleVersion //branch
+
+                    baseImageName = ("springboot-echo-service-").toLowerCase()
+//                    rcImageName = "${baseImageName}-rc-${shortCommit}-${buildNumber}"
+
+
+                    echo("building base image:" + baseImageName)
+
+                    echo "ENV"
+                    environment = env.getEnvironment()
+//                            .collect { envvar -> "${envvar.key}=${envvar.value}" }.join("\n")
+                    echo environment
+
+                }
+            }
+        }
+
+        stage('Authenticate to project') {
+            steps {
+                script {
+                    sessionToken = ocStartSession()
+                    useProject(projectId)
+                    sourceRepoCredentials = "openshift:\\" + sessionToken
+                }
+            }
+        }
+
+        stages {
+            stage('Build App') {
+                steps {
+                    echo "building application"
+                    sh "mvn install"
+                }
+            }
+        }
+        stage('Build image') {
+            steps {
+                script {
+//                    createImageBuild(rcImageName, buildNumber)
+                    runImageBuild(rcImageName)
+                }
+            }
+        }
+    }
+}
+
+def runImageBuild(def imageName) {
+    sh script: "oc start-build bc/${imageName}-image --wait --follow"
+}
+
+//def createImageBuild() {
+//    sh script: "set +x; oc process -f build.yml" + " | oc create -f -"
+//}
+
+def ocStartSession() {
+    println("Requesting OC session")
+    def sessionToken = sh script: "set +x; oc sa get-token nate -n cicd", returnStdout: true
+    sh script: "set +x; oc login kubernetes.default.svc --insecure-skip-tls-verify --token=$sessionToken"
+    return sessionToken
+}
+
+def useProject(def project) {
+    println("Switching to project ${project}")
+    sh script: "set +x; oc project ${project}"
+}
